ee9a5f8851815b8972d5c9264b308566739ca3ef
hc5 <yzhou0728@gmail.com>
2012-01-11 23:26:13 -0500

    Now prints the best sequence as well

--- src/Folder.java
+++ src/Folder.java
@@ -2,9 +2,9 @@
 public class Folder {
 
 	public static void main(String [] args){
-		Sequence s = new Sequence("GGGAAAUCC");
-		s.fold();
+		Sequence s = new Sequence("GGGAAGGGGCGCCUAUUCC");
 		System.out.println(s.getMaxPairings());
+		System.out.println(s.getFoldedStruct());
 		
 	}
 }
--- src/Sequence.java
+++ src/Sequence.java
@@ -1,10 +1,11 @@
 
 public class Sequence {
-	private final boolean DEBUG = false;
+	private final boolean DEBUG = true;
 	private String basepairs;
 	private String foldedStruct;
 	private int maxPairings;
 	private int [][] completedMatrix = null;
+	private String [][] completeStructMatrix = null;
 	public String getBasepairs() {
 		return basepairs;
 	}
@@ -14,25 +15,30 @@ public class Sequence {
 		this.foldedStruct = "";
 		this.maxPairings = -1;
 		this.completedMatrix = null;
+		this.completeStructMatrix = null;
 	}
 
 	public Sequence(String basepairs) {
 		this.basepairs = basepairs.toLowerCase();
 	}
 	public String getFoldedStruct(){
+		if(this.completedMatrix==null)
+			fold();
 		return foldedStruct;
 	}
 	public int getMaxPairings(){
+		if(this.completedMatrix==null)
+			fold();
 		return maxPairings;
 	}
 	public void fold(){
-		int [][] s = getPairingMatrix();
-		maxPairings = s[0][s.length-1];
-		print(s);
-		
+		getPairingMatrix();
+		maxPairings = completedMatrix[0][completedMatrix.length-1];
+		foldedStruct = completeStructMatrix[0][completeStructMatrix.length-1];
 	}
 	private void print(int[][]a){
 		if(!DEBUG)return;
+		System.out.println("~~~~~");
 		for(int i =0;i<a.length;i++){
 			for(int j =0;j<a[0].length;j++){
 				//System.out.print("start:"+i+","+"end:"+j+"-"+a[i][j]+"  ");
@@ -42,43 +48,58 @@ public class Sequence {
 		}
 	}
 
-	private int [][] getPairingMatrix(){
+	private void getPairingMatrix(){
 		int [][] m = new int [basepairs.length()][basepairs.length()];
+		String [][] optimalStructs = new String [basepairs.length()][basepairs.length()];
 		//initialization
 		for(int i = 0;i<m.length;i++){
 			m[i][i]=0;
+			optimalStructs[i][i]=".";
 			if(i>0){
 				m[i][i-1]=0;
+				optimalStructs[i][i-1]="";
 			}
 			if(i<m.length-1){
-				m[i][i+1]=(isValidPair(i,i+1)?1:0);
+				if(isValidPair(i,i+1)){
+					m[i][i+1]=1;
+					optimalStructs[i][i+1]="()";
+				}
 			}
 		}
 		for(int end=1;end<m.length;end++){
 			for(int start =end-1;start>-1;start--){
 				int max = 0;
+				
+				//worst case is all '.' for the entire sequence, no basepairing
+				String bestStruct = new String(new char[end-start+1]).replace("\0", ".");;
 				if(isValidPair(start,end)){
 					if(m[start+1][end-1]+1>max)
 						max = m[start+1][end-1]+1;
-					System.out.println(start+":"+end+" is valid pair "+max);
+						bestStruct = "("+optimalStructs[start+1][end-1]+")";
 				}
 
-				if(m[start+1][end]>max)
+				if(m[start+1][end]>max){
 					max = m[start+1][end];
-				if(m[start][end-1]>max)
+					bestStruct = "."+optimalStructs[start+1][end];
+				}
+				if(m[start][end-1]>max){
 					max = m[start][end-1];
+					bestStruct = optimalStructs[start][end-1]+".";
+				}
 				for(int i =start+1;i<end;i++){
 					int curBpCount = m[start][i]+m[i+1][end];
-					if(curBpCount>max)
+					if(curBpCount>max){
 						max = curBpCount;
+						bestStruct = optimalStructs[start][i]+optimalStructs[i+1][end];
+					}
 				}
 
 				m[start][end]=max;
-				print(m);
+				optimalStructs[start][end] = bestStruct;
 			}
 		}
 		this.completedMatrix=m;
-		return m;
+		this.completeStructMatrix=optimalStructs;
 	}
 	private boolean isValidPair(int x,int y){
 		char b1 = basepairs.charAt(x);
