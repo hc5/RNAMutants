dfd6f1797d8f59915a45bcffc56e127559806ef3
yzhou <yzhou0728@gmail.com>
2012-01-19 16:41:26 -0500

    added maccaskill partitioning algorithm

--- src/rna/Folder.java
+++ src/rna/Folder.java
@@ -3,7 +3,8 @@ package rna;
 public class Folder {
 
 	public static void main(String [] args){
-		Sequence s = new Sequence("AUGC");
+		Sequence s = new Sequence("AUGCAUUUGUUUGACC");
+		s.setBeta(0.5);
 		System.out.println(s.getMaxPairings());
 		System.out.println(s.getFoldedStruct());
 		
--- src/rna/Sequence.java
+++ src/rna/Sequence.java
@@ -6,7 +6,17 @@ public class Sequence {
 	private String foldedStruct;
 	private int maxPairings;
 	private int [][] completedMatrix = null;
+	private double beta;
+	
 	private String [][] completeStructMatrix = null;
+	private double [][] partitionMatrix;
+	public double getBeta() {
+		return beta;
+	}
+
+	public void setBeta(double beta) {
+		this.beta = beta;
+	}
 	public String getBasepairs() {
 		return basepairs;
 	}
@@ -37,7 +47,47 @@ public class Sequence {
 		getPairingMatrix();
 		maxPairings = completedMatrix[0][completedMatrix.length-1];
 		foldedStruct = completeStructMatrix[0][completeStructMatrix.length-1];
+		partitionMatrix = calculatePartitionMatrix();
 	}
+
+	public double [][] calculatePartitionMatrix(){
+		double [][] pmatrix = new double[completedMatrix.length][completedMatrix.length];
+		for(int end=1;end<pmatrix.length;end++){
+			for(int start =end-1;start>-1;start--){
+				double curSum = 0.0;
+				if(isValidPair(start,end)){
+					curSum += energyFunction(completedMatrix[start+1][end-1]) * energyFunction(1);
+				}
+				curSum += energyFunction(completedMatrix[start][end-1]);
+				curSum += energyFunction(completedMatrix[start+1][end]);
+				for(int k =start+1;k<end;k++){
+
+					curSum += energyFunction(completedMatrix[start][k]) * energyFunction(completedMatrix[k+1][end]);
+				
+				}
+				pmatrix[start][end] = curSum;
+			}
+		}
+		return pmatrix;
+	}
+
+	private void print(double[][] a) {
+		if(!DEBUG)return;
+		System.out.println("~~~~~");
+		for(int i =0;i<a.length;i++){
+			for(int j =0;j<a[0].length;j++){
+				//System.out.print("start:"+i+","+"end:"+j+"-"+a[i][j]+"  ");
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	//override this to change
+	public double energyFunction(int bp){
+		return Math.exp(this.beta * bp * -1);
+	}
+	
 	private void print(int[][]a){
 		if(!DEBUG)return;
 		System.out.println("~~~~~");
@@ -71,13 +121,13 @@ public class Sequence {
 		for(int end=1;end<m.length;end++){
 			for(int start =end-1;start>-1;start--){
 				int max = 0;
-				
+
 				//worst case is all '.' for the entire sequence, no basepairing
-				String bestStruct = new String(new char[end-start+1]).replace("\0", ".");;
+				String bestStruct = new String(new char[end-start+1]).replace("\0", ".");
 				if(isValidPair(start,end)){
 					if(m[start+1][end-1]+1>max)
 						max = m[start+1][end-1]+1;
-						bestStruct = "("+optimalStructs[start+1][end-1]+")";
+					bestStruct = "("+optimalStructs[start+1][end-1]+")";
 				}
 
 				if(m[start+1][end]>max){
@@ -103,6 +153,14 @@ public class Sequence {
 		this.completedMatrix=m;
 		this.completeStructMatrix=optimalStructs;
 	}
+
+	public double [][] getPartitionMatrix(){
+		if(partitionMatrix == null){
+			this.fold();
+		}
+		return this.partitionMatrix;
+	}
+
 	private boolean isValidPair(int x,int y){
 		char b1 = basepairs.charAt(x);
 		char b2 = basepairs.charAt(y);
